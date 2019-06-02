2db261916ada61e354f34ff228989ec9972abcf1
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:05 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,11 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostHOFConan(base.BoostBaseConan):
     name = "boost_hof"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_hof"
-    lib_short_names = ["hof"]
-    header_only_libs = ["hof"]
+    version = "1.70.0"
